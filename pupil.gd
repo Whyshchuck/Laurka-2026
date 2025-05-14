fd0837084a31ba97dd485b28cc8d656ca5c10e13
Whyshchuck <sz.lajszczak@gmail.com>
2025-05-14 23:22:51 +0200

    pupil logic scheme drafted

---
+++ pupil.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+@export var seat_position: Vector2
+
+func select_direction():
+	pass
+	
+func runa_away():
+	pass
+	
+
+
+
+
+
+func _on_button_pressed() -> void:
+	pass # Replace with function body.

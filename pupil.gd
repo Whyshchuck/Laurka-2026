212f4ae4cdeb8f4a969d5842442f7e955974666c
Jacek Wolszczak <jacek.wolszczak@demicon.com>
2025-06-06 19:36:43 +0200

    Use enum for character state.

--- pupil.gd
+++ pupil.gd
@@ -1,10 +1,17 @@
 extends CharacterBody2D
 
+enum CharacterState {
+	STOPPED,
+	MOVING,
+	RETURNING,
+	LOCKED
+}
+
 @export var speed := 300.0
 
+var character_state: CharacterState = CharacterState.STOPPED
+
 var direction := Vector2.ZERO
-var locked := false
-var returning_to_start := false
 var start_position := Vector2.ZERO
 var nav_map_rid: RID
 @onready var agent := $NavigationAgent2D
@@ -13,12 +20,13 @@ func _ready():
 	start_position = global_position
 	var nav_region: NavigationRegion2D = get_node("/root/Classroom/NavigationRegion2D")
 	nav_map_rid = nav_region.get_navigation_map()
-	
+
 	pick_new_target()
 
-func _input(event):
-	if locked or returning_to_start:
+func _unhandled_input(event: InputEvent) -> void:
+	if character_state in [CharacterState.LOCKED, CharacterState.RETURNING]:
 		return
+		
 	if event is InputEventMouseButton and event.pressed:
 		var mouse_pos: Vector2 = get_global_mouse_position()
 		var rect: Rect2 = get_node("TextureRect").get_global_rect()
@@ -26,16 +34,15 @@ func _input(event):
 			return_to_start()
 
 func _physics_process(delta):
-	if locked:
+	if character_state == CharacterState.LOCKED:
 		direction = Vector2.ZERO
 		global_position = start_position
 		return
 
-	if returning_to_start:
+	if character_state == CharacterState.RETURNING:
 		move_along_agent(delta)
 		if global_position.distance_to(start_position) < 5:
-			locked = true
-			returning_to_start = false
+			character_state = CharacterState.LOCKED
 			direction = Vector2.ZERO
 		return
 
@@ -53,11 +60,12 @@ func move_along_agent(_delta):
 
 func pick_new_target():
 	agent.target_position = NavigationServer2D.map_get_random_point(nav_map_rid, 1, false)
+	character_state = CharacterState.MOVING
 
 func return_to_start():
-	returning_to_start = true
+	character_state = CharacterState.RETURNING
 	agent.target_position = start_position
 
 func is_available() -> bool:
 	"""Check that pupil is not locked or returning"""
-	return not locked and not returning_to_start
+	return character_state == CharacterState.STOPPED

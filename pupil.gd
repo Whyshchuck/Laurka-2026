d33a71872f1b4c6ee442d1b641e0bbf219d9f0f1
Jacek Wolszczak <jacek.wolszczak@demicon.com>
2025-06-20 11:44:36 +0200

    Limit the number of pupil respawns and increase the respawn timeout on each respawn.

--- pupil.gd
+++ pupil.gd
@@ -9,17 +9,21 @@ enum CharacterState {
 
 @export var speed := 300.0
 
-var character_state: CharacterState = CharacterState.STOPPED
+const COLOR_NORMAL := Color(1, 1, 1, 1)
+const COLOUR_RESPAWN := Color(1, 0.3, 0.3, 1)
+const RESPAWN_TIMEOUT := 5
+const MAX_RESPAWN_COUNT = 3
 
+var character_state: CharacterState = CharacterState.STOPPED
 var direction := Vector2.ZERO
 var start_position := Vector2.ZERO
 var nav_map_rid: RID
+var respawn_count := 0
+
 @onready var agent := $NavigationAgent2D
 @onready var respawn_timer : Timer= $RespawnTimer
 @onready var texture_rect : TextureRect = $TextureRect
 
-var normal_colour := Color(1, 1, 1, 1)
-var respawning_colour := Color(1, 0.3, 0.3, 1)
 
 func _ready():
 	start_position = global_position
@@ -31,23 +35,25 @@ func _unhandled_input(event: InputEvent) -> void:
 	if character_state in [CharacterState.LOCKED, CharacterState.RETURNING]:
 		return
 		
-	if event is InputEventMouseButton and event.pressed:
-		
-		var mouse_pos: Vector2 = get_global_mouse_position()
-		var rect: Rect2 = $TextureRect.get_global_rect()
-		if rect.has_point(mouse_pos):
-			if Global.current_mode == Global.GameMode.EASY:
+	if not (event is InputEventMouseButton and event.pressed):
+		return
+	
+	
+	var mouse_pos: Vector2 = get_global_mouse_position()
+	if texture_rect.get_global_rect().has_point(mouse_pos):
+		match Global.current_mode:
+			Global.GameMode.EASY:
 				if $AudioStreamPlayer:
 					$AudioStreamPlayer.play()
-			
-			if Global.current_mode == Global.GameMode.HARD:
+		
+			Global.GameMode.HARD:
 				return_to_start()
 
 func _physics_process(delta):
 	if character_state == CharacterState.STOPPED:
 		return
 	
-	#z_index = 10+self.position.y
+	z_index = 10+self.position.y
 	if character_state == CharacterState.LOCKED:
 		direction = Vector2.ZERO
 		global_position = start_position
@@ -58,14 +64,16 @@ func _physics_process(delta):
 		if global_position.distance_to(start_position) < 5:
 			character_state = CharacterState.LOCKED
 			direction = Vector2.ZERO
-			respawn_timer.start()
+			
+			respawn_timer.start(RESPAWN_TIMEOUT + respawn_count)
+			respawn_count += 1
 		return
 
 	if agent.is_navigation_finished():
 		pick_new_target()
-	if Global.current_mode == Global.GameMode.EASY:
-		return
-	move_along_agent(delta)
+		
+	if Global.current_mode == Global.GameMode.HARD:
+		move_along_agent(delta)
 
 func move_along_agent(_delta):
 	var next_pos = agent.get_next_path_position()
@@ -82,20 +90,20 @@ func return_to_start():
 	agent.target_position = start_position
 
 func is_available() -> bool:
-	"""Check that pupil is not locked or returning"""
+	"""True if the character is stopped."""
 	return character_state == CharacterState.STOPPED
 
 func is_moving() -> bool:
-	"""Check that pupil is not locked or returning"""
+	"""True if the character is actively moving."""
 	return character_state == CharacterState.MOVING
 
 func _on_respawn_timer_timeout():
-	if character_state == CharacterState.LOCKED:
+	if character_state == CharacterState.LOCKED and respawn_count <= MAX_RESPAWN_COUNT:
 		blink()
-		await get_tree().create_timer(1.0).timeout  # Wait for blink to finish
+		await get_tree().create_timer(0.6).timeout
 		pick_new_target()
 
 func blink():
 	var tween := create_tween()
-	tween.tween_property(texture_rect, "modulate", respawning_colour, 0.3)
-	tween.tween_property(texture_rect, "modulate", normal_colour, 0.3)
+	tween.tween_property(texture_rect, "modulate", COLOUR_RESPAWN, 0.3)
+	tween.tween_property(texture_rect, "modulate", COLOR_NORMAL, 0.3)

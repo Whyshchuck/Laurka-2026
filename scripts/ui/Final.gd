fd0d96b5bc195c0d44578cfb9881060536a132ec
Jacek Wolszczak <jacek.wolszczak@outlook.com>
2026-06-10 13:12:44 +0200

    Small fixes to Final scene which is probably not used anymore

--- scripts/ui/Final.gd
+++ scripts/ui/Final.gd
@@ -5,12 +5,13 @@ extends Control
 func _ready() -> void:
 	restart_button.pressed.connect(_on_restart_button_pressed)
 	$AudioTadam.play()
-	if Global.current_mode == Global.GameMode.QUIZ and Global.time_elapsed:
-		var minutes := int(Global.time_elapsed) / 60
-		var seconds := int(Global.time_elapsed) % 60
-		var milliseconds := int((Global.time_elapsed - int(Global.time_elapsed)) * 1000)
+	if GameState.current_mode == GameState.GameMode.QUIZ and GameState.time_elapsed:
+		var time_elapsed: float = GameState.time_elapsed
+		var minutes := int(time_elapsed) / 60
+		var seconds := int(time_elapsed) % 60
+		var milliseconds := int((time_elapsed - int(time_elapsed)) * 1000)
 
 		$ResultLabel.text = "Twój wynik to: " + "%02d:%02d.%03d" % [minutes, seconds, milliseconds]
 	
 func _on_restart_button_pressed() -> void:
-	Global.go_to_mode_selection()
+	SceneManager.go_to_mode_selection()
